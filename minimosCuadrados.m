64b837ecca30f5da8af67f55d06c44fbeb1c59c0
pacoDan1 <31734608+pacoDan1@users.noreply.github.com>
2017-11-09 18:19:41 -0300

    Update and rename minimosCuadrados to minimosCuadrados.m

---
+++ minimosCuadrados.m
@@ -0,0 +1,98 @@
+
+%abcisas = input("ingrese abcisas \n");
+%ordenadas = input ("ingrese ordenadas \n");
+abcisas = [ 1 2 3 ];
+ordenadas=[1 2 1.2 ];
+cantAbcisas = length(abcisas);
+hold on ;
+sprintf("cantidad de puntos ingresados es : %f",cantAbcisas);
+sx1=0;
+sx2=0;
+sx3=0;
+sx4=0;
+sy1=0;
+sy2=0;
+sy3=0;
+sy4=0;
+sfxPorx=0;
+sfxPorx2=0;
+mediaX=0;
+mediaY=0;
+sLnDeLaImagen=0;
+sLnDex=0;
+sLnDeLaImagenPorLaImagen=0;
+sLnDexAl2=0;
+sLnDeLaImagenPorLnDex=0;
+sDexiSobreyi = 0;
+sDeInversaDeyi=0;
+cantDePuntos=length(abcisas);
+
+for i=1:cantDePuntos
+  xi=abcisas(i);
+  yi=ordenadas(i);
+    sx1=sx1+xi;
+    sy1=sy1+yi;
+    sx2=sx2+(xi^2);
+    sx3=sx3+(xi^3);
+    sx4=sx4+(xi^4);
+    sfxPorx=sfxPorx+xi*yi;
+    sfxPorx2=sfxPorx2+xi*xi*yi;
+    sLnDeLaImagen=sLnDeLaImagen+log(yi);
+    sLnDeLaImagenPorLaImagen=sLnDeLaImagenPorLaImagen+log(yi)*yi;
+    sLnDeLaImagenPorLnDex=sLnDeLaImagenPorLnDex+log(yi)*log(xi);
+    sLnDex = sLnDex+log(xi);
+    sLnDexAl2 = sLnDexAl2+(log(xi)*log(xi));
+    sDexiSobreyi = sDexiSobreyi+ (xi/yi);
+    sDeInversaDeyi = sDeInversaDeyi +(1/yi);
+endfor
+matrizNormalParaLaParabola = [sx4 sx3 sx2 ;
+                              sx1 sx2 sx3  ;
+                              sx2 sx3  cantDePuntos];
+matrizNormalParaLaExponencial=[sx2 sx1;
+                              sx1 cantDePuntos]; 
+matrizNormalParaLaRecta = [sx2 sx1;
+                           sx1 cantDePuntos];
+matrizNormalParaLaPotencial = [sLnDexAl2 sLnDex;
+                                sLnDex cantDePuntos];
+matrizNormalParaLaHomografica = [sx2 sx1;
+                                  sx1 cantDePuntos];
+
+terminosIndependientesDeLaParabola = [sfxPorx2;sfxPorx;sx1] ; %matriz de un vector columna
+terminosIndependientesDeLaExponencial = [sLnDeLaImagenPorLaImagen;sLnDeLaImagen];
+terminosIndependientesDeRecta = [sfxPorx;sy1];
+terminosIndependientesDeLaHomografica = [sDexiSobreyi;sDeInversaDeyi];
+terminosIndependientesDeLaPotencial = [sLnDeLaImagenPorLnDex;sLnDeLaImagen];
+coeficientesDeLaParabola  = inv(matrizNormalParaLaParabola)*terminosIndependientesDeLaParabola;%[a;b;c] para ax^2+bx+c
+coeficientesDeLaExponencial = inv(matrizNormalParaLaExponencial)*terminosIndependientesDeLaExponencial; % [a;ln(b)]
+coeficientesDeLaRecta = inv(matrizNormalParaLaRecta)*terminosIndependientesDeRecta;%[a;b]
+coeficientesDeLaPotencial = inv(matrizNormalParaLaPotencial)*terminosIndependientesDeLaPotencial;%[a,ln(b)]
+coeficientesDeLaHomografica = inv(matrizNormalParaLaHomografica)*terminosIndependientesDeLaHomografica; %[1/a;b/a]
+parabola_a = coeficientesDeLaParabola(1,1);
+parabola_b = coeficientesDeLaParabola(2,1);
+parabola_c = coeficientesDeLaParabola(3,1);
+exponencial_a = coeficientesDeLaExponencial(1,1);
+exponencial_b  = e^(coeficientesDeLaExponencial(2,1));                    
+recta_a = coeficientesDeLaRecta(1,1);
+recta_b = coeficientesDeLaRecta(2,1);
+potencial_a = coeficientesDeLaPotencial(1,1);
+potencial_b = e.^coeficientesDeLaPotencial(2,1);      
+homografica_a = 1/coeficientesDeLaHomografica(1,1);
+homografica_b = homografica_a*coeficientesDeLaHomografica(2,1);
+x = (abcisas(1,1):0.1:abcisas(1,cantDePuntos));
+x_2=x;x_3=x_2;x_4=x_3;x_5=x_4;
+parabola_polinomio = parabola_a*(x.^2)+(parabola_b*x)+parabola_c;
+exponencial_funcion = exponencial_b*(e.^(exponencial_a*x));
+recta_funcion=recta_a*x+recta_b;
+potencial_funcion = potencial_b*(x.^(potencial_a));
+homografica_funcion = homografica_a./(x+homografica_b);
+%plot(abcisas,ordenadas);
+plot(x,parabola_polinomio,'r','linewidth',1);
+plot(x,exponencial_funcion,'g');% g de green
+plot(x,recta_funcion,'k','linewidth',2);%negro
+plot(x,potencial_funcion,'m');%magenta
+plot(x,homografica_funcion,'b');%blue
+title("Aproximaciones ");
+xlabel("<----------Abcisas------------------>");
+ylabel("<------------ordenadas------------->");
+legend('parabola','exponencial','recta','potencial','homografica');
+print('minimos cuadrados.png','-dpng');
